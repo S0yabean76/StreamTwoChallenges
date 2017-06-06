28ae117c25bded063ffa76fce437d8f363c5a217
Abby Sawyer <abbysawyer@blueyonder.co.uk>
2017-06-06 19:13:35 +0100

    Adding challenges

--- 02_Testing/03_Do The Simplest Thing That Works/vending_machine.py
+++ 02_Testing/03_Do The Simplest Thing That Works/vending_machine.py
@@ -1,9 +1,9 @@
-coins = [100, 50, 20, 10, 5, 2, 1]
+coins = [1, .50, .20, .10, .05, .02, .01]
 
 def give_change(amount):
     change = []
     for coin in coins:
-        if coin <= amount:
+        while coin <= amount:
             amount -= coin
             change.append(coin)
     return change
